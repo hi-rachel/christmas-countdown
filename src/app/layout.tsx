d1f48645e5e482649d4fa665037763b85c055172
hi-rachel <rachel.uiux@gmail.com>
2025-02-09 23:49:02 +0900

    feat: Metadata 적용, favicon 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,22 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Christmas Countdown - How many days until Christmas?",
+  description:
+    "Check how many days until Christmas! Count down to the special moment with our real-time counter.",
+  icons: {
+    icon: [
+      {
+        url: "/tree.svg",
+        type: "image/svg+xml",
+      },
+    ],
+  },
+};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
