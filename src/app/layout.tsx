3c83b2145efe5f65f158edb5138f79b1c586e37a
hi-rachel <rachel.uiux@gmail.com>
2025-02-10 00:16:55 +0900

    feat: Metadata openGraph, keywords 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,28 @@ export const metadata: Metadata = {
       },
     ],
   },
+  keywords: ["Christmas", "D-day", "크리스마스", "Christmas Countdown"],
+  openGraph: {
+    title: "Christmas Countdown",
+    description: "How many days until Christmas?",
+    url: "https://christmas-countdown-12-25.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "/main.png",
+        width: 1200,
+        height: 630,
+        alt: "Christmas Countdown",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Christmas Countdown",
+    description: "How many days until Christmas?",
+    site: "https://christmas-countdown-12-25.vercel.app/",
+    images: "/main.png",
+  },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
